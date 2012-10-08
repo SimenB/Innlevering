d235fab63095b8df4ffea75970ec6803d7f38133
Simen Bekkhus <sbekkhus91@gmail.com>
2012-10-08 19:12:26 +0200

    La til snake-klasse, og copyright-info i StyleCop-fila

---
+++ UltimateSnake/UltimateSnake/Snake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateSnake
+{
+    public class Snake
+    {
+        /// <summary>
+        /// The instance of the snake (singleton)
+        /// </summary>
+        private static Snake instance;
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="Snake"/> class from being created.
+        /// </summary>
+        private Snake()
+        {
+        }
+
+        /// <summary>
+        /// Gets the instance. If it's not initialized, do so
+        /// </summary>
+        public static Snake Instance
+        {
+            get
+            {
+                return instance == null ? instance : instance = new Snake();
+            }
+        }
+    }
+}
