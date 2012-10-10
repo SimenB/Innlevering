097c694f51284bbc30cc46feef97d5b3d7046ccf
Simen Bekkhus <sbekkhus91@gmail.com>
2012-10-10 19:12:17 +0200

    Kommenterte ut ikke-fungerende input-handler

--- UltimateSnake/UltimateSnake/InputHandler.cs
+++ UltimateSnake/UltimateSnake/InputHandler.cs
@@ -6,66 +6,66 @@
 //   Defines the InputHandler type.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
-
-namespace UltimateSnake
-{
-    public class InputHandler
-    {
-        #region Fields
-        KeyboardState KeyState = new KeyboardState();
-        Keys[] currentkeys;
-        Keys[] previouskeys;
-        #endregion
-
-        #region Update
-        /// <summary>
-        /// Updates the current keyboard state, and performs the appropriate actions according to the 
-        /// currently pressed buttons.
-        /// </summary>
-        public void Update()
-        {
-            this.currentkeys = this.KeyState.GetPressedKeys();
-            if (this.previouskeys == this.currentkeys)
-            {
-                goto end;
-            }
-            for (int i = 0; i < this.currentkeys.Length; i++)
-            {
-                switch (this.currentkeys[i])
-                {
-                    case Keys.W:
-                        ActionW();
-                        break;
-                    case Keys.A:
-                        ActionA();
-                        break;
-                    case Keys.S:
-                        ActionS();
-                        break;
-                    case Keys.D:
-                        ActionD();
-                        break;
-                    case Keys.Space:
-                        ActiSpace();
-                        break;
-                    case Keys.Left:
-                        ActionLeft();
-                        break;
-                    case Keys.Right:
-                        ActionRight();
-                        break;
-                    case Keys.Up:
-                        ActionUp();
-                        break;
-                    case Keys.Down:
-                        ActionDown();
-                        break;
-                }
-            }
-            end:
-            this.previouskeys = this.currentkeys;
-        }
-        #endregion
-        
-    }
-}
+// 
+// namespace UltimateSnake
+// {
+//     public class InputHandler
+//     {
+//         #region Fields
+//         KeyboardState KeyState = new KeyboardState();
+//         Keys[] currentkeys;
+//         Keys[] previouskeys;
+//         #endregion
+// 
+//         #region Update
+//         /// <summary>
+//         /// Updates the current keyboard state, and performs the appropriate actions according to the 
+//         /// currently pressed buttons.
+//         /// </summary>
+//         public void Update()
+//         {
+//             this.currentkeys = this.KeyState.GetPressedKeys();
+//             if (this.previouskeys == this.currentkeys)
+//             {
+//                 goto end;
+//             }
+//             for (int i = 0; i < this.currentkeys.Length; i++)
+//             {
+//                 switch (this.currentkeys[i])
+//                 {
+//                     case Keys.W:
+//                         ActionW();
+//                         break;
+//                     case Keys.A:
+//                         ActionA();
+//                         break;
+//                     case Keys.S:
+//                         ActionS();
+//                         break;
+//                     case Keys.D:
+//                         ActionD();
+//                         break;
+//                     case Keys.Space:
+//                         ActiSpace();
+//                         break;
+//                     case Keys.Left:
+//                         ActionLeft();
+//                         break;
+//                     case Keys.Right:
+//                         ActionRight();
+//                         break;
+//                     case Keys.Up:
+//                         ActionUp();
+//                         break;
+//                     case Keys.Down:
+//                         ActionDown();
+//                         break;
+//                 }
+//             }
+//             end:
+//             this.previouskeys = this.currentkeys;
+//         }
+//         #endregion
+//         
+//     }
+// }
